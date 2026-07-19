7dc7be6ef43031f9e6d27ce14734120002ffc54c
Magnificat Marie Augusta Umutesi <umutesimagnificat@gmail.com>
2026-07-19 22:48:35 +0200

    add else block

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -9,14 +9,15 @@ import sys
 def top_ten(subreddit):
     if len(sys.argv) < 2:
         print(None)
-    url = 'https://www.reddit.com/dev/api/r/{}/hot.json'.format(
-        subreddit
-    )
-    headers = {"User-Agent": "Mozilla/5.0"}
-    res = requests.get(url, headers=headers, allow_redirects=False)
-    if res.status_code != 200:
-        print(None)
     else:
-        data = json.loads(res.text)["data"]["children"]
-        for post in data[:10]:
-            print(post["data"]["title"])
+        url = 'https://www.reddit.com/dev/api/r/{}/hot.json'.format(
+            subreddit
+        )
+        headers = {"User-Agent": "Mozilla/5.0"}
+        res = requests.get(url, headers=headers, allow_redirects=False)
+        if res.status_code != 200:
+            print(None)
+        else:
+            data = json.loads(res.text)["data"]["children"]
+            for post in data[:10]:
+                print(post["data"]["title"])
