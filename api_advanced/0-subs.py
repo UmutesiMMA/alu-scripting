ef6ea5c4d305cb43684128a200064ac0539b9e17
Magnificat Marie Augusta Umutesi <umutesimagnificat@gmail.com>
2026-07-19 21:41:39 +0200

    answer how many subs

---
+++ api_advanced/0-subs.py
@@ -0,0 +1,19 @@
+
+import json
+import sys
+import requests
+
+if __name__ == '__main__':
+    def number_of_subscribers(subreddit):
+        if len(sys.argv) < 2:
+            return 0
+        url = 'https://www.reddit.com/dev/api/r/{}/about'.format(
+            subreddit
+        )
+        headers = {"User-Agent": "Mozilla/5.0"}
+        res = requests.get(url, headers=headers, allow_redirects=False)
+        if res.status_code != 200:
+            return 'nope'
+        body = json.loads(res.text)
+        return body['data']['subscribers']
+
